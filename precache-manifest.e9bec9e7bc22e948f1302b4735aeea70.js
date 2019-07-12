cf30f38704fdeb6c0178381c7c0233a11372e71c
HieuDo <minhhieu76qng@gmail.com>
2019-07-12 10:37:46 +0700

    Updates

---
+++ precache-manifest.e9bec9e7bc22e948f1302b4735aeea70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d0cf425dcea75cd68316a76e30ed03",
+    "url": "/the-venue/index.html"
+  },
+  {
+    "revision": "63f23696e7cd42fcd801",
+    "url": "/the-venue/static/css/main.6c958b7d.chunk.css"
+  },
+  {
+    "revision": "79926f5e383c523b8339",
+    "url": "/the-venue/static/js/2.956ace8c.chunk.js"
+  },
+  {
+    "revision": "63f23696e7cd42fcd801",
+    "url": "/the-venue/static/js/main.31789a21.chunk.js"
+  },
+  {
+    "revision": "54c36fd328112b8457f9",
+    "url": "/the-venue/static/js/runtime~main.f0c0552e.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/the-venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/the-venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/the-venue/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
